1144bbdbd64c48c17002a416872e52e6bf40ee45
Emiliano Arozqueta <earozquetal@c5.cdmx.gob.mx>
2026-01-09 12:16:17 -0600

    Agregar archivos de proyecto.

---
+++ CalculadoraAPI/Controllers/CalculadoraController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        [HttpPost("Suma")]
+        public ActionResult<CalculadoraRes> Suma([FromBody] CalculadoraRequest request)
+        {
+            var resultado = new CalculadoraRes
+            {
+                R = request.N1 + request.N2,
+                Operacion = $"{request.N1} + {request.N2} = {request.N1 + request.N2}"
+            };
+            return Ok(resultado);
+        }
+
+        [HttpPost("Resta")]
+        public ActionResult<CalculadoraRes> Resta([FromBody] CalculadoraRequest request)
+        {
+            var resultado = new CalculadoraRes
+            {
+                R = request.N1 - request.N2,
+                Operacion = $"{request.N1} + {request.N2} = {request.N1 - request.N2}"
+            };
+            return Ok(resultado);
+        }
+    }
+
+    public class CalculadoraRequest
+    {
+        public double N1 { get; set; }
+        public double N2 { get; set; }
+    }
+
+    public class CalculadoraRes
+    {
+        public double R { get; set; }
+        public string Operacion { get; set; }
+    }
+}
